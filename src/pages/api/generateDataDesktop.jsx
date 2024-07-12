c1859839ea4876c1300447849bce725cd4eb41e6
Alexis <d-alexis@hotmail.fr>
2024-07-12 22:34:47 +0200

    maj

--- src/pages/api/generateDataDesktop.jsx
+++ src/pages/api/generateDataDesktop.jsx
@@ -1,5 +1,5 @@
 import { launch } from "chrome-launcher";
-// import lighthouse from "lighthouse";
+import lighthouse from "lighthouse";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 
@@ -52,86 +52,84 @@ export default async function handler(req, res) {
       }
     }
 
-    // // Fonction pour générer les scores à partir de Lighthouse
-    // const generateScores = async (url, options) => {
-    //   const runnerResult = await lighthouse(url, options);
+    // Fonction pour générer les scores à partir de Lighthouse
+    const generateScores = async (url, options) => {
+      console.log('Starting Lighthouse...');
+      const runnerResult = await lighthouse(url, options);
 
-    //   const performance = runnerResult.lhr.categories.performance.score * 100;
-    //   const seo = runnerResult.lhr.categories.seo.score * 100;
-    //   const bestPractices =
-    //     runnerResult.lhr.categories["best-practices"].score * 100;
-    //   const accessibility =
-    //     runnerResult.lhr.categories.accessibility.score * 100;
+      const performance = runnerResult.lhr.categories.performance.score * 100;
+      const seo = runnerResult.lhr.categories.seo.score * 100;
+      const bestPractices =
+        runnerResult.lhr.categories["best-practices"].score * 100;
+      const accessibility =
+        runnerResult.lhr.categories.accessibility.score * 100;
 
-    //   return { performance, seo, bestPractices, accessibility };
-    // };
+      return { performance, seo, bestPractices, accessibility };
+    };
 
-    // try {
-    //   chrome = await launch({ chromeFlags: ["--headless"] });
+    try {
+      chrome = await launch({ chromeFlags: ["--headless"] });
 
-    //   const desktopOptions = {
-    //     logLevel: "info",
-    //     output: "html",
-    //     onlyCategories: [
-    //       "performance",
-    //       "accessibility",
-    //       "best-practices",
-    //       "seo",
-    //     ],
-    //     port: chrome.port,
-    //     formFactor: "desktop",
-    //     screenEmulation: {
-    //       mobile: false,
-    //       width: 1350,
-    //       height: 940,
-    //       deviceScaleFactor: 1,
-    //       disabled: false,
-    //     },
-    //   };
+      const desktopOptions = {
+        logLevel: "info",
+        output: "html",
+        onlyCategories: [
+          "performance",
+          "accessibility",
+          "best-practices",
+          "seo",
+        ],
+        port: chrome.port,
+        formFactor: "desktop",
+        screenEmulation: {
+          mobile: false,
+          width: 1350,
+          height: 940,
+          deviceScaleFactor: 1,
+          disabled: false,
+        },
+      };
 
-    //   if (userId) {
-    //     // Récupérer uniquement l'ID du dernier rapport créé
-    //     lastReport = await prisma.report.findFirst({
-    //       orderBy: { createdAt: "desc" },
-    //       select: { id: true },
-    //     });
+      if (userId) {
+        // Récupérer uniquement l'ID du dernier rapport créé
+        lastReport = await prisma.report.findFirst({
+          orderBy: { createdAt: "desc" },
+          select: { id: true },
+        });
 
-    //     if (!lastReport) {
-    //       throw new Error("Aucun rapport trouvé.");
-    //     }
-    //     console.log("Dernier rapport ID:", lastReport.id);
-    //   }
+        if (!lastReport) {
+          throw new Error("Aucun rapport trouvé.");
+        }
+        console.log("Dernier rapport ID:", lastReport.id);
+      }
 
-    //   const desktopScores = await generateScores(url, desktopOptions);
+      const desktopScores = await generateScores(url, desktopOptions);
 
-    //   // Enregistrer les scores uniquement si l'utilisateur est connecté
-    //   if (userId) {
-    //     await prisma.desktopPerformanceScore.create({
-    //       data: {
-    //         reportId: lastReport.id, // Utilisez l'ID du dernier rapport
-    //         performance: desktopScores.performance,
-    //         seo: desktopScores.seo,
-    //         accessibility: desktopScores.accessibility,
-    //         bestpractices: desktopScores.bestPractices,
-    //       },
-    //     });
-    //   }
+      // Enregistrer les scores uniquement si l'utilisateur est connecté
+      if (userId) {
+        await prisma.desktopPerformanceScore.create({
+          data: {
+            reportId: lastReport.id, // Utilisez l'ID du dernier rapport
+            performance: desktopScores.performance,
+            seo: desktopScores.seo,
+            accessibility: desktopScores.accessibility,
+            bestpractices: desktopScores.bestPractices,
+          },
+        });
+      }
 
-    //   res.status(200).json(desktopScores);
-    // } catch (error) {
-    //   console.error("Erreur lors de l'analyse Lighthouse:", error);
-    //   res.status(500).json({
-    //     error:
-    //       "Erreur lors de la génération des données de performance desktop.",
-    //   });
-    // } finally {
-    //   if (chrome) {
-    //     await chrome.kill();
-    //   }
-    // }
-
-    res.status(200).json({ message: 'Traitement rapide réussi' });
-
+      res.status(200).json(desktopScores);
+    } catch (error) {
+      console.error("Erreur lors de l'analyse Lighthouse:", error);
+      res.status(500).json({
+        error:
+          "Erreur lors de la génération des données de performance desktop.",
+      });
+    } finally {
+      if (chrome) {
+        await chrome.kill();
+      }
+    }
   } else {
     res.status(405).json({ error: "Méthode non autorisée." });
   }
