6ef5ad320766916368149ea97aed19f30e03a7da
Alexis <d-alexis@hotmail.fr>
2024-07-29 20:36:48 +0200

    maj

--- .lighthouserc.desktop.js
+++ .lighthouserc.desktop.js
@@ -1,23 +1,23 @@
 module.exports = {
-	ci: {
-        collect: {
-            url: ['https://www.flexbeton.fr/'],
-            startServerCommand: 'npm run start',
-            numberOfRuns: 3,
-            settings: {
-              preset: 'desktop',
-            },
-          },
-          upload: {
-            target: 'temporary-public-storage',
-          },
-          assert: {
-            assertions: {
-              'categories:performance': ['error', {minScore: 0.9}],
-              'categories:accessibility': ['error', {minScore: 0.9}],
-              'categories:best-practices': ['error', {minScore: 0.9}],
-              'categories:seo': ['error', {minScore: 0.9}],
-            },
-          },
-        },
-      };
+  ci: {
+    collect: {
+      url: ['https://www.flexbeton.fr/'],
+      startServerCommand: 'npm run start',
+      numberOfRuns: 3,
+      settings: {
+        preset: 'desktop',
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', {minScore: 0.9}],
+        'categories:accessibility': ['error', {minScore: 0.9}],
+        'categories:best-practices': ['error', {minScore: 0.9}],
+        'categories:seo': ['error', {minScore: 0.9}],
+      },
+    },
+  },
+};
--- .lighthouserc.mobile.js
+++ .lighthouserc.mobile.js
@@ -1,24 +1,24 @@
 module.exports = {
-    ci: {
-      collect: {
-        url: ['https://www.flexbeton.fr/'],
-        startServerCommand: 'npm run start',
-        numberOfRuns: 3,
-        settings: {
-          preset: 'mobile',
-        },
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
-      assert: {
-        assertions: {
-          'categories:performance': ['error', {minScore: 0.9}],
-          'categories:accessibility': ['error', {minScore: 0.9}],
-          'categories:best-practices': ['error', {minScore: 0.9}],
-          'categories:seo': ['error', {minScore: 0.9}],
-        },
+  ci: {
+    collect: {
+      url: ['https://www.flexbeton.fr/'],
+      startServerCommand: 'npm run start',
+      numberOfRuns: 3,
+      settings: {
+        preset: 'perf', 
+        emulatedFormFactor: 'mobile' 
       },
     },
-  };
-  
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', {minScore: 0.9}],
+        'categories:accessibility': ['error', {minScore: 0.9}],
+        'categories:best-practices': ['error', {minScore: 0.9}],
+        'categories:seo': ['error', {minScore: 0.9}],
+      },
+    },
+  },
+};
