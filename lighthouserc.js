7cb92eac95a1a7d6cded29175df826f6f19d446b
Alexis <d-alexis@hotmail.fr>
2024-07-29 13:24:30 +0200

    configuration

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,15 @@
 module.exports = {
     ci: {
       collect: {
-        staticDistDir: "./public/",
+        url: [process.env.COLLECT_URL]
       },
       assert: {
         preset: 'lighthouse:no-pwa',
         assertions: {
             'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }], 
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
           },
       },
       upload: {
