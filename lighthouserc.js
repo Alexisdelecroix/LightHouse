359d7765131128706b2457637bc99a71b54855b6
Alexis <d-alexis@hotmail.fr>
2024-07-29 11:51:51 +0200

    insert URL

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,9 +2,13 @@ module.exports = {
     ci: {
       collect: {
         staticDistDir: './public', // Mettez ici le chemin vers le répertoire contenant vos fichiers statiques
+        url: ['https://www.flexbeton.fr/']
       },
       assert: {
         preset: 'lighthouse:no-pwa',
+        assertions: {
+            'categories:performance': ['error', { minScore: 0.9 }],
+          },
       },
       upload: {
         target: 'temporary-public-storage',
