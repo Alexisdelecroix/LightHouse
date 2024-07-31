80dfea39564fc56f44e2c3598b310460663c8085
Alexis <d-alexis@hotmail.fr>
2024-07-31 11:10:59 +0200

    maj

--- src/app/auth/login/route.js
+++ src/app/auth/login/route.js
@@ -1,35 +1,35 @@
-import bcrypt from 'bcryptjs'
-import { PrismaClient } from '@prisma/client'
-import jwt from 'jsonwebtoken'
+// import bcrypt from 'bcryptjs'
+// import { PrismaClient } from '@prisma/client'
+// import jwt from 'jsonwebtoken'
 
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function POST(request) {
+// export async function POST(request) {
 
-    try {
-        // Récupération des données du corps de la requête
-        const { email, password } = await request.json();
+//     try {
+//         // Récupération des données du corps de la requête
+//         const { email, password } = await request.json();
 
-        // Recherche de l'utilisateur dans la base de données par email
-        const user = await prisma.user.findUnique({
-            where: { email },
-        });
+//         // Recherche de l'utilisateur dans la base de données par email
+//         const user = await prisma.user.findUnique({
+//             where: { email },
+//         });
 
-        if (user && bcrypt.compareSync(password, user.password)) {
-            // Génération d'un token JWT
-            const token = jwt.sign(
-                { id: user.id, email: user.email }, // Payload du token
-                process.env.JWT_SECRET, // Clé secrète pour signer le token
-                { expiresIn: '1h' } // Expiration du token
-            );
-            return NextResponse.json({ token })
-        }
-        else {
-            return NextResponse.json({ message: 'Email ou password invalide' }, { status: 401 });
-        }
-    } catch (error) {
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    }
-}
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//             // Génération d'un token JWT
+//             const token = jwt.sign(
+//                 { id: user.id, email: user.email }, // Payload du token
+//                 process.env.JWT_SECRET, // Clé secrète pour signer le token
+//                 { expiresIn: '1h' } // Expiration du token
+//             );
+//             return NextResponse.json({ token })
+//         }
+//         else {
+//             return NextResponse.json({ message: 'Email ou password invalide' }, { status: 401 });
+//         }
+//     } catch (error) {
+//         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+//     }
+// }
--- src/app/auth/register/route.js
+++ src/app/auth/register/route.js
@@ -1,89 +1,89 @@
-import bcrypt from 'bcryptjs';
-import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
+// import bcrypt from 'bcryptjs';
+// import { PrismaClient } from '@prisma/client';
+// import { NextResponse } from 'next/server';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-// Fonction pour vérifier la force du mot de passe
-function isStrongPassword(password_admin) {
-    const lengthCheck = password_admin.length >= 8;
-    const lowercaseCheck = /[a-z]/.test(password_admin);
-    const uppercaseCheck = /[A-Z]/.test(password_admin);
-    const digitCheck = /\d/.test(password_admin);
-    const specialCharCheck = /[@$!%*?&]/.test(password_admin);
+// // Fonction pour vérifier la force du mot de passe
+// function isStrongPassword(password_admin) {
+//     const lengthCheck = password_admin.length >= 8;
+//     const lowercaseCheck = /[a-z]/.test(password_admin);
+//     const uppercaseCheck = /[A-Z]/.test(password_admin);
+//     const digitCheck = /\d/.test(password_admin);
+//     const specialCharCheck = /[@$!%*?&]/.test(password_admin);
 
-    return {
-        isValid: lengthCheck && lowercaseCheck && uppercaseCheck && digitCheck && specialCharCheck,
-        lengthCheck: lengthCheck,
-        lowercaseCheck: lowercaseCheck,
-        uppercaseCheck: uppercaseCheck,
-        digitCheck: digitCheck,
-        specialCharCheck: specialCharCheck
-    };
-}
+//     return {
+//         isValid: lengthCheck && lowercaseCheck && uppercaseCheck && digitCheck && specialCharCheck,
+//         lengthCheck: lengthCheck,
+//         lowercaseCheck: lowercaseCheck,
+//         uppercaseCheck: uppercaseCheck,
+//         digitCheck: digitCheck,
+//         specialCharCheck: specialCharCheck
+//     };
+// }
 
-export async function POST(request) {
-    try {
-        // Récupération des données du corps de la requête
-        const { email, password } = await request.json();
+// export async function POST(request) {
+//     try {
+//         // Récupération des données du corps de la requête
+//         const { email, password } = await request.json();
 
-        // Vérification de la force du mot de passe
-        const passwordValidation = isStrongPassword(password);
-        if (!passwordValidation.isValid) {
-            let errorMessage = 'Le mot de passe doit contenir';
+//         // Vérification de la force du mot de passe
+//         const passwordValidation = isStrongPassword(password);
+//         if (!passwordValidation.isValid) {
+//             let errorMessage = 'Le mot de passe doit contenir';
 
-            if (!passwordValidation.lengthCheck) {
-                errorMessage += ' au moins 8 caractères,';
-            }
+//             if (!passwordValidation.lengthCheck) {
+//                 errorMessage += ' au moins 8 caractères,';
+//             }
 
-            if (!passwordValidation.uppercaseCheck) {
-                errorMessage += ' au moins une majuscule,';
-            }
+//             if (!passwordValidation.uppercaseCheck) {
+//                 errorMessage += ' au moins une majuscule,';
+//             }
 
-            if (!passwordValidation.lowercaseCheck) {
-                errorMessage += ' au moins une minuscule,';
-            }
+//             if (!passwordValidation.lowercaseCheck) {
+//                 errorMessage += ' au moins une minuscule,';
+//             }
 
-            if (!passwordValidation.digitCheck) {
-                errorMessage += ' au moins un chiffre,';
-            }
+//             if (!passwordValidation.digitCheck) {
+//                 errorMessage += ' au moins un chiffre,';
+//             }
 
-            if (!passwordValidation.specialCharCheck) {
-                errorMessage += ' au moins un caractère spécial';
-            }
-            return NextResponse.json({ message: errorMessage }, { status: 400 })
-        }
-        try {
-            // Vérification si l'email existe déjà dans la base de données
-            const existingUser = await prisma.user.findUnique({
-                where: {
-                    email: email
-                }
-            });
+//             if (!passwordValidation.specialCharCheck) {
+//                 errorMessage += ' au moins un caractère spécial';
+//             }
+//             return NextResponse.json({ message: errorMessage }, { status: 400 })
+//         }
+//         try {
+//             // Vérification si l'email existe déjà dans la base de données
+//             const existingUser = await prisma.user.findUnique({
+//                 where: {
+//                     email: email
+//                 }
+//             });
 
-            if (existingUser) {
-                return NextResponse.json({ message: "Cette adresse e-mail est déjà utilisée" })
-            }
+//             if (existingUser) {
+//                 return NextResponse.json({ message: "Cette adresse e-mail est déjà utilisée" })
+//             }
 
-            // Génération du sel et hachage du mot de passe
-            const salt = bcrypt.genSaltSync(10); // Génère un sel avec un facteur de coût de 10
-            const hashedPassword = bcrypt.hashSync(password, salt); // Hache le mot de passe avec le sel généré
+//             // Génération du sel et hachage du mot de passe
+//             const salt = bcrypt.genSaltSync(10); // Génère un sel avec un facteur de coût de 10
+//             const hashedPassword = bcrypt.hashSync(password, salt); // Hache le mot de passe avec le sel généré
 
-            // Création d'un nouvel utilisateur dans la base de données
-            const newUser = await prisma.user.create({
-                data: {
-                    email, // Adresse email de l'utilisateur
-                    password: hashedPassword // Mot de passe haché
-                },
-            });
+//             // Création d'un nouvel utilisateur dans la base de données
+//             const newUser = await prisma.user.create({
+//                 data: {
+//                     email, // Adresse email de l'utilisateur
+//                     password: hashedPassword // Mot de passe haché
+//                 },
+//             });
 
-            return NextResponse.json({ newUser }, { status: 201 })
+//             return NextResponse.json({ newUser }, { status: 201 })
 
-        } catch (error) {
-            return NextResponse.json({ message: "Echec de la cration d'un utilisateur", error: error.message })
-        }
-    }
-    catch (error) {
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    }
-}
+//         } catch (error) {
+//             return NextResponse.json({ message: "Echec de la cration d'un utilisateur", error: error.message })
+//         }
+//     }
+//     catch (error) {
+//         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+//     }
+// }
